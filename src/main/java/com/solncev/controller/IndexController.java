3291790ee83a6924edfe6e0732dddb8701f7182a
Marat Solntsev <msolntsev@dev.apays.ru>
2026-04-18 09:39:54 +0300

    spring boot aop

--- src/main/java/com/solncev/controller/IndexController.java
+++ src/main/java/com/solncev/controller/IndexController.java
@@ -1,11 +1,13 @@
 package com.solncev.controller;
 
+import com.solncev.aop.Loggable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+    @Loggable
     @GetMapping(value = "index")
     public String index() {
         return "index";
